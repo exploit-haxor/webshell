00845d62dd5bb86261d539b87ca472e9942343f1
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-07 01:59:59 +0700

    Add files via upload

---
+++ readme.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<body>
+
+<audio autoplay>
+<source src="https://cvar1984.github.io/audio/moan.mp3">
+</audio>
+
+Untuk caranya langsung aja tonton vidionya cekidot:v
+https://youtu.be/_s6lN6v4a10
+<style>html{height:100%;margin:0;background-image:url(https://media1.tenor.com/m/J01NX0ikmNwAAAAC/ricardo-flick-dance.gif);background-position:center;background-repeat:no-repeat;background-size:cover}</style><div id=audioSection><audio autoplay controls id=bgAudio loop style=display:none><source src=https://cvar1984.github.io/audio/moan.mp3 type=audio/mpeg></audio><script>document.onkeydown=function(e){if(e.ctrlKey&&(85===e.keyCode||119===e.keyCode))return!1},window.addEventListener("beforeunload",function(e){e.preventDefault(),e.returnValue="Gacor Kang?"});var audio=document.getElementById("bgAudio")</script></div>
+
+<embed src="https://j.top4top.io/m_2967gtgnw1.mp3&autoplay=1" type="application/x-shockwave-flash"wmode="transparent" width="0" height="0"></embed>
+</head>
+</body>
+</html>
